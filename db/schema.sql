cfff89874c5060065d8f0a5567080c309b91815a
Walt Mankowski <waltman@pobox.com>
2010-11-02 23:55:40 -0400

    swapped nick_id and channel_id in messages table

--- db/schema.sql
+++ db/schema.sql
@@ -29,8 +29,8 @@ drop table if exists messages;
 create table messages (
        message_id integer not null primary key autoincrement,
        network_id integer,
-       nick_id integer,
        channel_id integer,
+       nick_id integer,
        message text,
        time_added integer NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
