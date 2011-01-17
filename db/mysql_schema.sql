715584b63ed3ce5f7ad0cf753e350be6c90c5867
Walt Mankowski <waltman@pobox.com>
2011-01-17 11:15:42 -0500

    made as many fields as possible 'not null'

--- db/mysql_schema.sql
+++ db/mysql_schema.sql
@@ -9,7 +9,7 @@ drop table if exists networks;
 
 create table networks (
        network_id integer unsigned not null primary key auto_increment,
-       network varchar(32) unique,
+       network varchar(32) not null unique,
        time_added timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) engine = innodb;
 
@@ -17,7 +17,7 @@ drop table if exists nicks;
 
 create table nicks (
        nick_id integer unsigned not null primary key auto_increment,
-       nick varchar(64) unique,
+       nick varchar(64) not null unique,
        time_added timestamp NOT NULL DEFAULT NOW()
 ) engine = innodb;
 
@@ -25,7 +25,7 @@ drop table if exists channels;
 
 create table channels (
        channel_id integer unsigned not null primary key auto_increment,
-       channel varchar(32) unique,
+       channel varchar(32) not null unique,
        time_added timestamp NOT NULL DEFAULT NOW()
 ) engine = innodb;
 
@@ -33,10 +33,10 @@ drop table if exists messages;
 
 create table messages (
        message_id integer unsigned not null primary key auto_increment,
-       network_id integer unsigned,
+       network_id integer not null unsigned,
        channel_id integer unsigned,
-       nick_id integer unsigned,
-       message varchar(4096),
+       nick_id integer not null unsigned,
+       message varchar(4096) not null,
        time_added timestamp NOT NULL DEFAULT NOW(),
        foreign key (network_id) references networks(network_id) on delete cascade,
        foreign key (channel_id) references channels(channel_id) on delete cascade,
